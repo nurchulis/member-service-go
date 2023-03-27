53ba3bb1c8dda5b675e16667dd331b6180bcb285
nurchulis <nura3609@gmail.com>
2023-03-27 15:18:47 +0700

    init

--- models/models.go
+++ models/models.go
@@ -1,8 +1,6 @@
 package models
 
 import (
-	"database/sql"
-	"fmt"
 	"go-postgres-crud/config"
 	"log"
 	"time"
@@ -18,50 +16,6 @@ type Book struct {
 	Image       string     `json:"image"`
 	CreatedAt   *time.Time `json:"created_at"`
 	UpdatedAt   *time.Time `json:"updated_at"`
-}
-
-type Image struct {
-	ID  int64  `json:"id"`
-	Url string `json:"url"`
-}
-
-type Asset struct {
-	ID        int64      `json:"id"`
-	Name      string     `json:"name"`
-	Url       string     `json:"url"`
-	CreatedAt *time.Time `json:"created_at"`
-	UpdatedAt *time.Time `json:"updated_at"`
-}
-
-func AddAssets(name string, url string) int64 {
-	db := config.CreateConnection()
-	defer db.Close()
-	sqlStatement := `INSERT INTO assets (name, url)
-	VALUES ($1, $2) RETURNING id`
-	var id int64
-	err := db.QueryRow(sqlStatement,
-		name, url).Scan(&id)
-	if err != nil {
-		log.Fatalf("Failed Exec query. %v", err)
-	}
-	fmt.Printf("Insert data single record %v", id)
-	return id
-}
-
-func AddBook(book Book) int64 {
-	db := config.CreateConnection()
-	defer db.Close()
-	sqlStatement := `INSERT INTO books (title, Description, Rating, Image)
-	VALUES ($1, $2, $3, $4) RETURNING id`
-	var id int64
-	err := db.QueryRow(sqlStatement,
-		book.Title, book.Description,
-		book.Rating, book.Image).Scan(&id)
-	if err != nil {
-		log.Fatalf("Failed Exec query. %v", err)
-	}
-	fmt.Printf("Insert data single record %v", id)
-	return id
 }
 
 // ambil satu book
@@ -84,57 +38,4 @@ func GetAllBook(title string) ([]Book, error) {
 		books = append(books, book)
 	}
 	return books, err
-}
-
-// mengambil satu book
-func GetBook(id int64) (Book, error) {
-	db := config.CreateConnection()
-	defer db.Close()
-	var book Book
-	sqlStatement := `SELECT * FROM books WHERE id=$1`
-	row := db.QueryRow(sqlStatement, id)
-	err := row.Scan(&book.ID, &book.Title, &book.Description, &book.Rating, &book.Image, &book.CreatedAt, &book.UpdatedAt)
-	switch err {
-	case sql.ErrNoRows:
-		fmt.Println("Data Not Found")
-		return book, nil
-	case nil:
-		return book, nil
-	default:
-		log.Fatalf("Cannot get Data. %v", err)
-	}
-	return book, err
-}
-
-// update user in the DB
-func UpdateBook(id int64, book Book) int64 {
-	db := config.CreateConnection()
-	defer db.Close()
-	sqlStatement := `UPDATE books SET title=$2, description=$3, rating=$4, image=$5 WHERE id=$1`
-	res, err := db.Exec(sqlStatement, id, book.Title, book.Description, book.Rating, book.Image)
-	if err != nil {
-		log.Fatalf("Failed Exec query. %v", err)
-	}
-	rowsAffected, err := res.RowsAffected()
-	if err != nil {
-		log.Fatalf("Error update. %v", err)
-	}
-	fmt.Printf("Success Update Data %v\n", rowsAffected)
-	return rowsAffected
-}
-
-func DeleteBook(id int64) int64 {
-	db := config.CreateConnection()
-	defer db.Close()
-	sqlStatement := `DELETE FROM books WHERE id=$1`
-	res, err := db.Exec(sqlStatement, id)
-	if err != nil {
-		log.Fatalf("Failed Exec query. %v", err)
-	}
-	rowsAffected, err := res.RowsAffected()
-	if err != nil {
-		log.Fatalf("Data not found. %v", err)
-	}
-	fmt.Printf("Success Delete Data%v", rowsAffected)
-	return rowsAffected
 }
--- router/router.go
+++ router/router.go
@@ -10,13 +10,7 @@ func Router() *mux.Router {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/books", controller.GetAllBook).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/book/{id}", controller.GetBook).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/book", controller.AddBook).Methods("POST", "OPTIONS")
-	router.HandleFunc("/api/book/{id}", controller.UpdateBook).Methods("PUT", "OPTIONS")
-	router.HandleFunc("/api/book/{id}", controller.DeleteBook).Methods("DELETE", "OPTIONS")
-	router.HandleFunc("/api/upload/assets", controller.UploadAsset).Methods("POST", "OPTIONS")
-	router.HandleFunc("/api/book-validation", controller.AddPageValidation).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/pubsub", controller.GetAllBook).Methods("GET", "OPTIONS")
 
 	return router
 }
